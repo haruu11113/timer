f1eb6ce98263850ea33ff9bd956ac39bf3c002d2
haruu11113 <haruu11113@gmail.com>
2020-11-29 18:36:08 +0900

    refactor(Uiをちょっときれいにした):

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,6 @@
 // import UUID from "uuid";
 import { Timer } from "./Timer";
+import { State } from "./State";
 
 // const contentsElem = document.getElementById('contents');
 
@@ -27,16 +28,13 @@ import { Timer } from "./Timer";
 // },1000);
 
 const timer: Timer = new Timer(25);
-console.log('init');
-console.log(timer.finishAt);
+
 var startbutton: HTMLInputElement = <HTMLInputElement>document.getElementById("start");
 startbutton.addEventListener('click', () => {
     let timerElem: HTMLInputElement = <HTMLInputElement>document.getElementById('timer');
     let minuteFormElem: HTMLInputElement = <HTMLInputElement>document.getElementById('workMinute');
+
     timer.updateFinishAt(parseInt(minuteFormElem.value));
-    // const timer: Timer = new Timer(parseInt(minuteFormElem.value));
-    console.log('updated');
-    console.log(timer.finishAt);
     timer.stopWork();
     timer.startWork();
     timerElem.innerText = String(timer.finishAt.getHours()) + ' : ' + String(timer.finishAt.getMinutes()) + ' : ' + String(timer.finishAt.getSeconds());
