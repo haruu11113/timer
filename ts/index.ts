d99b91c0c39986377a830b49e14ab04ca76e6666
haruu11113 <kanekoharu2a@gmail.com>
2020-11-29 18:14:31 +0900

    feat(startを二度押したときに時間お更新する):

--- ts/index.ts
+++ ts/index.ts
@@ -26,12 +26,18 @@ import { Timer } from "./Timer";
 //     contentsElem.innerText = timer.getClock(); 
 // },1000);
 
-
+const timer: Timer = new Timer(25);
+console.log('init');
+console.log(timer.finishAt);
 var startbutton: HTMLInputElement = <HTMLInputElement>document.getElementById("start");
 startbutton.addEventListener('click', () => {
     let timerElem: HTMLInputElement = <HTMLInputElement>document.getElementById('timer');
     let minuteFormElem: HTMLInputElement = <HTMLInputElement>document.getElementById('workMinute');
-    let timer: Timer = new Timer(parseInt(minuteFormElem.value));
+    timer.updateFinishAt(parseInt(minuteFormElem.value));
+    // const timer: Timer = new Timer(parseInt(minuteFormElem.value));
+    console.log('updated');
+    console.log(timer.finishAt);
+    timer.stopWork();
     timer.startWork();
     timerElem.innerText = String(timer.finishAt.getHours()) + ' : ' + String(timer.finishAt.getMinutes()) + ' : ' + String(timer.finishAt.getSeconds());
 
